afcb95d62b908bc1fae77abfb73880425ba7eb78
nazimboudeffa <boudeffa@gmail.com>
2021-08-23 00:21:34 +0200

    Added thumbs

---
+++ src/components/Thumb/index.js
@@ -0,0 +1,12 @@
+import { div } from 'prelude-ls';
+import React from 'react';
+//Styles
+import { Image } from './Thumb.styles';
+
+const Thumb = ({ image, movieId, clickable}) => (
+    <div>
+        <Image src={image} alt='movie-thumb' />
+    </div>
+)
+
+export default Thumb;
