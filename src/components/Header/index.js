d0db8f88c60ccdd3dadd07130b8bffeb080d9e02
nazimboudeffa <boudeffa@gmail.com>
2021-08-22 02:12:59 +0200

    First Commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import react from 'react';
+
+import BTLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
+
+const Header = () => (
+    <Wrapper>
+        <Content>
+            <LogoImg src={BTLogo} alt='bt-logo' />
+            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
+        </Content>
+    </Wrapper>
+);
+
+export default Header;
